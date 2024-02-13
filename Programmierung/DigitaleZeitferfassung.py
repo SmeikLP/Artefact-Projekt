d82d13c8d39c420be6e8c1f38b99cd5f32a18eef
Smeik <terranauticarocket@gmail.com>
2024-02-13 14:02:54 +0100

    Created new Script for monitoring work times

---
+++ Programmierung/DigitaleZeitferfassung.py
@@ -0,0 +1,88 @@
+
+import time
+
+WhatToDisplay = 0
+CurrentlySelected = ""
+MainStatusDisplayed = False
+MemberStatusDisplayed = False
+
+# Dictionary to keep track of the scan count for each EPC
+# https://www.w3schools.com/python/python_dictionaries.asp
+# Maybe Export Dictionary Values to SCV Sheet for working with Graphs and othet Data Managment. JSON might also be a valuable option
+MitarbeiterListe = {
+    "Paul": "Mitarbeiter01",
+    "Greta": "Mitarbeiter02",
+    "Gabriel": "Mitarbeiter03"
+}
+
+Mitarbeiter01 = {
+    "name": "Paul",
+    "Lastname": "Stanisch",
+    "weeklyWorktime": 39,
+    "currentTime": 0,
+    "ArrivedTodayAt": None,
+    "LeftWorkAt": None,
+    "Status": None,
+    "Overtime": 0,
+    "AvarageBreakTimeInMin": 45
+}
+Mitarbeiter02 = {
+    "name": "Greta",
+    "Lastname": "Stanisch",
+    "weeklyWorktime": 39,
+    "currentTime": 0,
+    "ArrivedTodayAt": None,
+    "LeftWorkAt": None,
+    "Status": None,
+    "Overtime": 0,
+    "AvarageBreakTimeInMin": 45
+}
+Mitarbeiter03 = {
+    "name": "Gabriell",
+    "Lastname": "Stanisch",
+    "weeklyWorktime": 39,
+    "currentTime": 0,
+    "ArrivedTodayAt": None,
+    "LeftWorkAt": None,
+    "Status": None,
+    "Overtime": 0,
+    "AvarageBreakTimeInMin": 45
+}
+
+def display_status():
+    print("\nMitarbeiter Übersicht:")
+    print_nested_dict_key(MitarbeiterListe)
+
+def display_worker():
+    print("\nMoin Moin\n")
+    print(MitarbeiterListe[CurrentlySelected])
+
+def print_nested_dict_key(dictionary):
+    for employee_id, employee_name in dictionary.items(): #Have no fucking clue what employee_id does but deleting it causes problems so it has to stay
+        employee_data = globals()[employee_name]
+        print(f"{employee_data['name']}: {employee_data['Status']}")
+
+display = 0
+
+while True:
+    match display:
+        case 0:
+            if MainStatusDisplayed == False:
+                display_status()
+                MainStatusDisplayed = True
+                MemberStatusDisplayed = False
+
+        case 1:
+            if MemberStatusDisplayed == False:
+                display_worker()
+                MainStatusDisplayed = False
+                MemberStatusDisplayed = True
+                time.sleep(20)
+                display = 0
+
+
+###https://www.conrad.de/de/p/tru-components-schreib-und-lesemodul-tc-9927144-1-st-2481786.html?hk=SEM&WT.mc_id=google_pla&gad_source=1&gclid=Cj0KCQiAw6yuBhDrARIsACf94RUnn15PIssUqNSlKl-n56Ix79LZuXAUmXzl73zZO-X-ECmsGpG1XiIaAmL8EALw_wcB
+            
+
+
+    
